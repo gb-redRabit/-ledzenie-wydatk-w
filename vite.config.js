75e61b3bc9cea0487fc67b19112bfe97243f2a2a
gb-redRabit <136236629+gb-redRabit@users.noreply.github.com>
2024-05-10 16:50:20 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
-  base: "/<REPO>/",
+  base: "/-ledzenie-wydatk-w/",
   plugins: [vue()],
   resolve: {
     alias: {
