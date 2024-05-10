b68513bb4e81c4a920ab2439cde333f4e796d786
gb-redRabit <136236629+gb-redRabit@users.noreply.github.com>
2024-05-10 16:35:10 +0200

    as

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
+  publicPath: process.env.NODE_ENV === "production" ? "/REPO_NAME/" : "/",
   plugins: [vue()],
   resolve: {
     alias: {
